8f60e4886e4a6d2dab41956c387d48f289e22712
Collins Liyosi <collinskivale@gmail.com>
2018-09-25 14:52:19 +0300

    Initialize project

---
+++ src/main/java/com/liyosi/fruitshop/FruitShopApplication.java
@@ -0,0 +1,12 @@
+package com.liyosi.fruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitShopApplication.class, args);
+	}
+}
